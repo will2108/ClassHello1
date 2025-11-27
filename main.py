24fd882561d00169ec7cf044abf3620741180a98
will2108 <willmail2someone@gmail.com>
2025-11-27 21:53:33 +0800

    改成77777

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root(name: str = "PyCharm3.2"):
+def read_root(name: str = "PyCharm777777"):
     """Root endpoint. Optional query param `name` (default: PyCharm)."""
     return {"message": f"Hi, {name}"}
 
